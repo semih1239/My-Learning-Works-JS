46d356f460a97fc1f936e3068a383acc773438eb
Semih <semihayyildiz23@hotmail.com>
2021-04-03 02:07:11 +0300

    Add files via upload

---
+++ 2-Variables-Types/script.js
@@ -0,0 +1,57 @@
+// Primitive Types
+
+// String
+
+let firstName='Semih';
+
+// console.log(typeof firstName)
+
+// Number
+
+let age= 20;
+let money = 100.5
+
+// console.log(typeof money);
+
+// Boolean
+
+let isActive= true
+
+// console.log(typeof isActive);
+
+// null
+
+let job=null
+
+// console.log(typeof job)
+
+// undefined
+
+let car ;
+
+// console.log(typeof car)
+
+// Reference Types - Object
+
+// Array
+
+let names=['Ali','Ahmet','Can']
+
+// console.log(typeof names)
+
+// Object
+
+let address = {
+    city:'Copenhagen',
+    country:'Denmark'
+}
+
+// console.log(typeof address)
+
+// Function
+
+var calculateAge = function(){
+    return 0;
+};
+
+console.log(typeof calculateAge)
