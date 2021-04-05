fabdfecd707f1d05ad0f1ec2fc1399bd5975a852
Semih <semihayyildiz23@hotmail.com>
2021-04-05 17:37:17 +0300

    Add files via upload

---
+++ 2-Variables&Types-Demo/script.js
@@ -0,0 +1,33 @@
+var customerName = 'Semih'
+var customerSurname = 'AYYILDIZ'
+var customerId = '123123123'
+var customerTotalOrder = 300.8
+var customerGender = 'male'
+var customerAdress = {
+    city : 'Copenhagen',
+    address: 'Abc street'
+}
+var customerHobbies = ['Cinema','Book','Spor']
+
+var order1='100';
+var order2='150';
+
+// console.log(Number(order1)+ Number(order2))
+
+var order3 = '100.2'
+var order4 = '150.5'
+
+// console.log(Number(order3)+Number(order4))
+
+var order5 = '100.2'
+var order6 = '150.5'
+
+// console.log(parseInt(order5) + parseInt(order6))
+
+const yearOfBirth = 1995
+
+// console.log(new Date().getFullYear()-yearOfBirth)
+
+let course = 'Modern Javascript Course'
+
+console.log(course.length)
