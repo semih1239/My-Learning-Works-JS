54d6ae023f9a2cabcb01b5d71565fb76ac7c1304
Semih <semihayyildiz23@hotmail.com>
2021-04-10 19:34:08 +0300

    Add files via upload

---
+++ 11-If&ElseStatements/script.js
@@ -0,0 +1,50 @@
+// if &  else statements
+
+const firstName = 'Semih'
+const age = 26
+const isStudent = true
+const school = 'university'
+
+// if(firstName == 'Semih'){
+//     console.log('Hello Semih')
+// }
+
+// if (age == 26){
+//     console.log('your age is 26')
+// }
+
+// if (isStudent){
+//     console.log('Hello Student')
+// }
+// else{
+//     console.log('Hello how is going')
+// }
+
+// if (age >= 18){
+//     if(school=='university'  || school == 'high school'){
+//     console.log('You can get a driver license')}
+//     else{
+//         console.log('Your education is not enought for license')
+//     }
+
+// }
+// else{
+//     console.log('You cant get a driver license')
+// }
+
+// if (age > 0 && age <12){
+//     console.log(`${firstName} is a chield`)
+// }
+// else if(age >= 13 && age <= 19){
+//     console.log(`${firstName} is a teenager`)
+// }
+// else{
+//     console.log(`${firstName} is an adult`)
+// }
+
+if (typeof id !== 'undefined'){
+    console.log('id: ' + id)
+}
+else{
+    console.log('No id')
+}
