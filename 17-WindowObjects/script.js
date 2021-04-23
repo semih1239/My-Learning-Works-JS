82d3bf6c6080523a576cbeadb17f61d4dd88c6fb
Semih <semihayyildiz23@hotmail.com>
2021-04-23 05:20:42 +0200

    Add files via upload

---
+++ 17-WindowObjects/script.js
@@ -0,0 +1,52 @@
+// Window Object
+
+// let val
+// let a = 10
+// function abc(){
+//     return 0
+// }
+
+// val = window
+
+// // alert
+// alert('Merhaba')
+
+// prompt
+
+// let b = prompt('Write a number')
+
+// confirm
+
+// let val = confirm('Are you sure?')
+// console.log(val)
+// if (val){
+//     console.log('ok')
+// }
+// else{
+//     console.log('no')
+// }
+
+// scrool    scroolbarin pixel olarak konumunu gosterir
+// let = window.scrollX
+// let = window.scrollY
+
+// location yuklenen siteyle alakali alt ozelliklere ulasabiliriz
+
+val = window.location
+val = window.location.href
+val = window.location.hostname
+val = window.location.host
+val = window.location.protocol
+val = window.location.search
+
+// window.location.href= 'http://semihayyildiz.com'
+// window.location.reload()  sayfayi yeniler
+
+
+console.log(val)
+
+
+// navigator
+
+
+// window.document html dosyasini verir
