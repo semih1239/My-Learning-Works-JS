723469e77242d0f4f3bcdf0bca7a72d7a52203ce
Semih <semihayyildiz23@hotmail.com>
2021-05-02 18:50:57 -0700

    Add files via upload

---
+++ 0-MyDemos/FruitsArray/script.js
@@ -0,0 +1,30 @@
+let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+let animals=["Cat", "Dog", "Elephon", "Rabid"]
+// 1 ) fruits arrayini araya cizgi (-) koyarak ekrana yazdiriniz
+// 2 ) animals arrayindeki son elemani silebilirmisiniz
+// 3 ) fruits arayine (kivi) ekleyebilirmisiniz
+// 4 ) animals arrayinin ilk ogesini silebilirmisini
+// 5 ) fruits arrayinden (lemon, apple,mango) elemanlarini silip Kiwi ekleyebilirmisiniz
+// 6 ) fruits ve animals araylerini birlestirebilirmisiniz
+// 7 ) iki arayin son elemanlarinin index numaralarini ekrana yazdirabilirmisiniz
+
+
+// console.log(fruits.join('-'))
+
+// animals.pop()
+// console.log(animals)
+
+// fruits.push('kivi')
+// console.log(fruits)
+
+// animals.shift()
+// console.log(animals)
+
+// fruits.splice(2,3,'kivi')
+// console.log(fruits)
+
+// let total =fruits.concat(animals)
+// console.log(total)
+
+// console.log(fruits.length-1)
+// console.log(animals.length-1)
