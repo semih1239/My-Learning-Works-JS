4a68e36a560c6ac2111c2c6ab142224138c5de9d
Semih <semihayyildiz23@hotmail.com>
2021-09-12 17:12:43 +0300

    Add files via upload

---
+++ 01-DOM-Methods/5-CreatingElements/script.js
@@ -0,0 +1,31 @@
+// Creating Element
+
+// create element
+
+const li = document.createElement('li')
+
+// add class
+li.className='list-group-item list-group-item-secondary'
+
+li.setAttribute('title','new item') //li etiketine title ekler new item title
+li.setAttribute('data-id','5')
+
+
+// text node
+
+const text = document.createTextNode('new item')
+li.appendChild(text)
+
+const a = document.createElement('a')
+a.setAttribute('href','#')
+a.className ='delete-item float-right'
+a.innerHTML='<i class="fas fa-times"></i>'
+
+// append a to li
+
+li.appendChild(a)
+
+// append li to ul
+document.querySelector('#task-list').appendChild(li)
+
+console.log(li)
